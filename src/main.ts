e8bb27168b1b015b8c613c264492d59ed498e0ee
Linshen Xiao <githublsx@users.noreply.github.com>
2018-03-20 22:59:20 -0400

    build

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,6 @@ function readTextFile(file: string): string
 // Define an object with application parameters and button callbacks
 // This will be referred to by dat.GUI's functions that add GUI elements.
 const controls = {
-  tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
   cameracontrol: false,
   camerarotation: true,
